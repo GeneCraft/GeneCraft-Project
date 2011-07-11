a4e661bda08846dbfaeb1464a8a53038069278d3
slvlirnoff <slvlirnoff@gmail.com>
2011-07-11 15:01:01 +0000

    worker working again
    git-svn-id: http://www.genecraft-project.com/svn/genecraft/trunk@168 2ce2bb41-2ff2-4fd5-96a4-e3c75a7bd90e

--- src/genelab_worker/main.cpp
+++ src/genelab_worker/main.cpp
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
     Entity* e;
     btWorldFactory* worldFactory = new btWorldFactory();
     btShapesFactory* shapesFactory = new btShapesFactory();
-
+    float max = 0;
     btWorld* world = worldFactory->createWorld(factory, shapesFactory, worldFactory->createSimpleWorld());
 
     for(int i = 0; i < MAX_ENTITY; i++) {
@@ -74,6 +74,91 @@ int main(int argc, char *argv[])
     }
 
     qDebug() << "lancement des engines !";
+    int cpt = 0;
+    bool stable = false;
+    int needStableCpt = 0;
+    float lastHeight = 0;
+    while(1) {
+        cpt++;
+        if(!stable && cpt < 600) {
+            QList<Entity*> entities = ee->getAllEntities();
+            foreach(Entity* e, entities) {
+                StatisticsProvider* stat = e->getStatistics().find("FixationStats").value();
+                stat->step();
+                Statistic* s = e->getStatisticByName("Root relative velocity");
+                if(lastHeight + EPSILON > s->getValue() &&
+                   lastHeight - EPSILON < s->getValue()) {
+                    needStableCpt++;
+                } else {
+                    needStableCpt = 0;
+                }
+                lastHeight = s->getValue();
+
+                if(needStableCpt > 60) {
+                    stable = true;
+                    s->resetAll();
+                    needStableCpt = 0;
+                }
+            }
+            ((BulletEngine*)engines.find("Bullet").value())->beforeStep();
+            ((BulletEngine*)engines.find("Bullet").value())->step();
+            ((BulletEngine*)engines.find("Bullet").value())->afterStep();
+            continue;
+        }
+
+        //qDebug() << cpt;
+        foreach(Engine* e, engines) {
+
+            e->beforeStep();
+        }
+
+        foreach(Engine* e, engines) {
+
+            e->step();
+        }
+
+        foreach(Engine* e, engines) {
+
+            e->afterStep();
+        }
+        if(cpt%60 == 0) {
+            //qDebug() << "\r" << cpt/60 << "secondes";
+        }
+
+        if(cpt >= MAX_TIME) {
+            qDebug() << "new entity !";
+            cpt = 0;
+            // Evaluation
+            QList<Entity*> entities = ee->getAllEntities();
+            if(stable)
+            foreach(Entity* e, entities) {
+                Statistic* s = e->getStatisticByName("Root relative velocity");
+                qDebug() << s->getSum();
+                if(s->getSum() > max) {
+                    max = s->getSum();
+                    qDebug() << "! new max " << s->getSum();
+                    //r->save(e->serialize());
+                }
+            }
+
+            foreach(Entity* e, entities) {
+                ee->removeEntity(e);
+                delete e;
+            }
+
+
+            for(int i = 0; i < MAX_ENTITY; i++) {
+
+                EntityFamily *spider = new SpiderFamily();
+                btVector3 pos = world->getSpawnPosition();
+                e = spider->createEntity(shapesFactory, pos);
+                e->setup();
+                ee->addEntity(e);
+            }
+
+            stable = false;
+        }
+    }
 
     return a.exec();
 }
